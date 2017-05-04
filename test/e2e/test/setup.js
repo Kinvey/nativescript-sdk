1dd3cac057fada9e9dbbb2e9d8243d047ed2b1bc
Thomas Conner <thomas@kinvey.com>
2017-05-04 16:44:50 -0400

    Remove unused tests

--- test/e2e/test/setup.js
+++
@@ -1,7 +0,0 @@
-import find from 'lodash/filter';
-
-before(function() {
-  const ctxs = browser.contexts();
-  const webviewCtx = find(ctxs.value, ctx => ctx.indexOf('WEBVIEW') === 0);
-  browser.context(webviewCtx);
-});
--- test/unit/index.js
+++ test/unit/index.js
@@ -1,47 +1,18 @@
-import Kinvey from 'src/index.js';
-import { randomString } from 'kinvey-js-sdk/dist/utils';
-import { NetworkRack, SerializeMiddleware, ParseMiddleware } from 'kinvey-js-sdk/dist/request';
+import { Kinvey } from 'kinvey-js-sdk';
 import nock from 'nock';
-import { UserMock, HttpMiddlewareMock } from './mocks';
-
-// Setup network rack
-NetworkRack.reset();
-NetworkRack.use(new SerializeMiddleware());
-NetworkRack.use(new HttpMiddlewareMock());
-NetworkRack.use(new ParseMiddleware());
 
 // Record for nock
 // nock.recorder.rec();
 
 // Init Kinvey
 before(function() {
-  // return Kinvey.initialize({
-  //   appKey: 'kid_HkTD2CJc',
-  //   appSecret: 'cd7f658ed0a548dd8dfadf5a1787568b'
-  // }).then(() => {
-  //   this.client = Kinvey.client;
-  // });
-
-  return Kinvey.initialize({
-    appKey: randomString(),
-    appSecret: randomString()
-  }).then(() => {
-    this.client = Kinvey.client;
+  Kinvey.init({
+    appKey: 'kid_HkTD2CJc',
+    appSecret: 'cd7f658ed0a548dd8dfadf5a1787568b'
   });
 });
 
-// Clean up
-after(function() {
-  delete this.client;
-});
-
-// Login a user
-beforeEach(() => UserMock.login('test', 'test'));
-
-// Logout the active user
-afterEach(() => UserMock.logout());
-
-// Clean up nock
+// Clear nock
 afterEach(function() {
   nock.cleanAll();
 });
