613eeab4c4007a48d4035ce4fbd29bcf3ae5101a
Georgi Prodanov <georgi.prodanov@progress.com>
2017-07-25 18:02:20 +0300

    Add files to .gitignore and small refactoring.

--- demo/app/app.component.ts
+++ demo/app/app.component.ts
@@ -6,7 +6,6 @@ import * as fs from "tns-core-modules/file-system";
 import * as imageSource from 'tns-core-modules/image-source';
 import * as platform from 'tns-core-modules/platform';
 import * as http from 'http';
-var bghttp = require("nativescript-background-http");
 
 const log = (msg, obj?) => {
   if (typeof msg !== 'string') {
@@ -33,19 +32,13 @@ export class AppComponent {
   }
 
   run() {
-    let collection, id = 'testttt';
     const activeUser = Kinvey.User.getActiveUser();
     let prm: Promise<any> = Promise.resolve();
     if (!activeUser) {
       prm = Kinvey.User.login('admin', 'admin');
     }
 
-    let fileContent, filePath;
-
     prm
-      .then(res => {
-        collection = Kinvey.DataStore.collection('Books', Kinvey.DataStoreType.Cache);
-      })
       .then(() => {
         return Kinvey.Files.find();
       })
@@ -56,34 +49,15 @@ export class AppComponent {
         });
         return Promise.all(prms);
       })
-      // .then(() => {
-      //   const savePath = fs.path.join(fs.knownFolders.temp().path, 'testfile.png');
-      //   return http.getFile('https://www.queness.com/resources/images/png/apple_web.png', savePath);
-      // })
       .then((wasSaved) => {
-        log('was saved: ', wasSaved);
-        // const file = fs.knownFolders.temp().getFile('testfile.png');
-        // filePath = file.path;
-        // const file = fs.File.fromPath('/data/data/test.test.test/files/app/images/bigimg.jpgz');
-        // return file;
         const filePath = fs.path.join(fs.knownFolders.currentApp().path, './images/bigimg.jpg');
-        console.log('exists: ' + fs.File.exists(filePath));
+        log('exists: ' + fs.File.exists(filePath));
         const file = fs.File.fromPath(filePath);
         return file;
       })
       .then((file) => {
         log('uploading');
-        // fileContent = file;
-
-        // const metadata = {
-        //   filename: 'testfile.png',
-        //   mimeType: 'image/png',
-        //   size: 23413956
-        // };
-        // return Kinvey.Files.upload('/data/data/test.test.test/files/app/images/bigimg.jpg', metadata);
-
-        fileContent = file.readSync();
-        console.log('size: ' + fileContent.length);
+        const fileContent = file.readSync();
 
         const metadata = {
           filename: 'testfile.jpg',
@@ -93,53 +67,8 @@ export class AppComponent {
         return Kinvey.Files.upload(file, metadata);
       })
       .then((resp: any) => {
-        const md = resp.data;
-        return md;
-        // return new Promise((resolve, reject) => {
-        //   http.request({
-        //     url: md._uploadURL,
-        //     method: 'PUT',
-        //     headers: {
-        //       'Content-Type': 'image/png',
-        //       'Content-Range': `bytes */${fileContent.length}`
-        //     }
-        //   })
-        //   .then(resp => {
-        //     if (resp.statusCode === 308) {
-        //       return resolve(md);
-        //     } else {
-        //       log('err resp: ' + resp.content.toString());
-        //       return reject({ message: 'Unexpected status code' });
-        //     }
-        //   });
-        // });
+        log('uploaded');
       })
-      // .then((fileMd: any) => {
-      //   var session = bghttp.session("image-upload");
-
-      //   var request = {
-      //     url: fileMd._uploadURL + 'aa',
-      //     method: "PUT",
-      //     headers: {
-      //       'Content-Type': 'image/png',
-      //       'Content-Range': `bytes 0-${fileContent.length - 1}/${fileContent.length}`
-      //     },
-      //     description: "{ 'uploading': 'testfilezzz.png' }"
-      //   };
-
-      //   var task = session.uploadFile(filePath, request);
-      //   const logEvent = ev => {
-      //     log(`${ev.eventName}: ${ev.currentBytes}/${ev.totalBytes}`);
-      //   };
-
-      //   task.on("progress", logEvent);
-      //   task.on("error", logEvent);
-      //   task.on("complete", logEvent);
-      //   task.on("responded", logEvent);
-      // })
-      // .then(res => {
-      //   log('got item: ', res);
-      // })
       .catch(err => {
         log('defEH: ', err.message);
       });
