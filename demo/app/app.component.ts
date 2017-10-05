f3fd21980ac798d3d23ca5de1113482c1cf58059
Thomas Conner <thomas@kinvey.com>
2017-10-05 17:59:00 -0400

    Revert "Remove demo directory"
    This reverts commit ea1cbf9b2a0d97dc9065cf0a7a4c229ecb1947a4.

---
+++ demo/app/app.component.ts
@@ -0,0 +1,80 @@
+declare const java: any;
+declare const org: any;
+
+import { Component } from '@angular/core';
+import { Kinvey } from 'kinvey-nativescript-sdk';
+import * as fs from 'tns-core-modules/file-system';
+import * as imageSource from 'tns-core-modules/image-source';
+import * as platform from 'tns-core-modules/platform';
+import * as http from 'http';
+
+const log = (msg, obj?) => {
+  if (typeof msg !== 'string') {
+    msg = JSON.stringify(msg, null, 2);
+  }
+  console.log('>>>>>>> ' + msg + (obj ? JSON.stringify(obj, null, 2) : ''));
+};
+
+@Component({
+  selector: 'my-app',
+  template: `
+    <ActionBar title="My App"></ActionBar>
+    <StackLayout>
+      <ActivityIndicator [busy]="true" horizontalAlignment="center" verticalAlignment="center"></ActivityIndicator>
+      <Label text="Run" (tap)="run()"></Label>
+    </StackLayout>
+    <!-- Your UI components go here -->
+  `
+})
+export class AppComponent {
+
+  constructor() {
+    Kinvey.init({
+      appKey: 'kid_HkTD2CJc',
+      appSecret: 'cd7f658ed0a548dd8dfadf5a1787568b'
+    });
+  }
+
+  run() {
+    const activeUser = Kinvey.User.getActiveUser();
+    let prm: Promise<any> = Promise.resolve();
+    if (!activeUser) {
+      prm = Kinvey.User.login('admin', 'admin');
+    }
+
+    prm
+      .then(() => {
+        return Kinvey.Files.find();
+      })
+      .then((files) => {
+        log('deleting');
+        const prms = files.map((f) => {
+          return Kinvey.Files.removeById(f._id);
+        });
+        return Promise.all(prms);
+      })
+      .then(() => {
+        const filePath = fs.path.join(fs.knownFolders.currentApp().path, './images/bigimg.jpg');
+        log('exists: ' + fs.File.exists(filePath));
+        const file = fs.File.fromPath(filePath);
+        return file;
+      })
+      .then((file) => {
+        log('uploading');
+        const fileContent = file.readSync();
+
+        const metadata = {
+          filename: file.name,
+          mimeType: 'image/jpg',
+          size: fileContent.length
+        };
+        return Kinvey.Files.upload(file, metadata, { timeout: 2147483647 });
+      })
+      .then((resp: any) => {
+        log('uploaded');
+      })
+      .catch((err) => {
+        log('defEH: ', err.message);
+      });
+  }
+}
