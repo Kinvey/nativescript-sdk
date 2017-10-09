1d399dd0198d0fea276d9c2edba2b71c9bc63acf
PROGRESS\tnedyalkov <tsvetomir.nedyalkov@progress.com>
2017-10-09 17:08:00 +0300

    Added externalConfig

--- test/template/main-page.js
+++ test/template/main-page.js
@@ -1,5 +1,6 @@
 var { tests, logServerPort } = require('./testConfig');
 var runner = require('./testRunner.bundle');
+externalConfig = require('./config.js');
 
 runner.initialize(tests, { logServerPort });
 runner.runAllTests();
