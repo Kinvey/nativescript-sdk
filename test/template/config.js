248f2f098a1cb13d6c9483a7c0604593d3bdfe56
PROGRESS\tnedyalkov <tsvetomir.nedyalkov@progress.com>
2017-11-15 13:11:26 +0200

    Added collectionName in config.js

--- test/template/config.js
+++ test/template/config.js
@@ -1,6 +1,7 @@
 externalConfig = {
     appKey: 'kid_H1fs4gFsZ',
-    appSecret: 'aa42a6d47d0049129c985bfb37821877'
+    appSecret: 'aa42a6d47d0049129c985bfb37821877',
+    collectionName: 'Books' 
 };
 
 
