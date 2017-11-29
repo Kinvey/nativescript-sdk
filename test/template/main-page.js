7ca986cd16e7e565328e69152c5b6111273d1664
PROGRESS\tnedyalkov <tsvetomir.nedyalkov@progress.com>
2017-11-29 14:39:09 +0200

    Added constants require  in the app template

--- test/template/main-page.js
+++ test/template/main-page.js
@@ -1,7 +1,8 @@
 var { tests, logServerPort } = require('./testConfig');
 var runner = require('./testRunner.bundle');
 externalConfig = require('./config.js');
-common = require('./tests/common.js');
+utilities = require('./tests/utilities.js');
+Constants = require('./tests/constants.js');
 Kinvey = require('kinvey-nativescript-sdk').Kinvey;
 runner.initialize(tests, { logServerPort });
 runner.runAllTests();
