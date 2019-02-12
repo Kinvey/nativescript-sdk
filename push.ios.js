edfc032928d8de5bf11411064607c12a0c592483
Travis CI User <travis@example.org>
2019-02-12 17:19:59 +0000

    Travis Build: 2575

--- push.ios.js
+++ push.ios.js
@@ -1,6 +1,6 @@
 /**
  * kinvey-nativescript-sdk - Kinvey NativeScript SDK for developing NativeScript applications.
- * @version v3.12.3
+ * @version v3.12.4
  * @author Kinvey, Inc.
  * @link http://www.kinvey.com
  * @license Apache-2.0
